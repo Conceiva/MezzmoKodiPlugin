f34bb0949428fd31da0b0ce97fe67c036d267985
Peter <peter_edens@yahoo.com.au>
2016-05-22 18:06:14 +1000

    updated

--- resources/lib/mezzmo.py
+++ resources/lib/mezzmo.py
@@ -4,7 +4,9 @@ import xbmcplugin
 import ssdp
 import xbmcaddon
 import xbmcgui
- 
+import urllib2
+import xml.etree.ElementTree
+
 def message(msg):
     __addon__ = xbmcaddon.Addon()
     __addonname__ = __addon__.getAddonInfo('name')
@@ -17,11 +19,19 @@ def start():
 
     servers = ssdp.discover("urn:schemas-upnp-org:device:MediaServer:1")
     xbmcplugin.setContent(addon_handle, 'movies')
-    message( servers )
+    
 
     for server in servers:
-        url = 'http://localhost/some_video.mkv'
-        li = xbmcgui.ListItem(server.location, iconImage='DefaultVideo.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-
+        url = server.location
+        response = urllib2.urlopen(url)
+        try:
+            e = xml.etree.ElementTree.parse(response).getroot()
+	    device = e.find('.//root/device/friendlyName')
+	    #friendlyname = e.find('friendlyName').text
+            message( device)
+            li = xbmcgui.ListItem(server.location, iconImage='DefaultVideo.png')
+            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+        except Exception as e:
+            message(e)
+            pass
     xbmcplugin.endOfDirectory(addon_handle)
